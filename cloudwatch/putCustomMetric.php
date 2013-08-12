9190545a27c814c01087941bd38aa5b7460a1d8a
root <root@php-app1.localdomain>
2013-08-12 18:30:36 +0000

    Added example for putting custom CloudWatch metrics

---
+++ cloudwatch/putCustomMetric.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(__DIR__ . '/../init.php');
+
+use Aws\CloudWatch\CloudWatchClient;
+
+$client = CloudWatchClient::factory(array(
+	'region' => 'us-east-1'
+));
+
+$client->putMetricData(array(
+	'Namespace' => 'Custom: Time Metrics',
+	'MetricData' => array(
+		array(
+			'MetricName' => 'Current Minute',
+			'Value' => date('s'),
+			'Unit' => 'Seconds',
+		),
+	)
+));
+
+?>
